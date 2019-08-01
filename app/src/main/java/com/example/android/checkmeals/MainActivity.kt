ce034069da157be561154a21af6e2672631b4843
Akash <akashmkabra@gmail.com>
2019-08-01 17:31:14 -0400

    using kotlin, creating new activities and use a click listener on textviews

--- app/src/main/java/com/example/android/checkmeals/MainActivity.kt
+++ app/src/main/java/com/example/android/checkmeals/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.android.checkmeals
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the textview with id monday
+        val monday = findViewById<TextView>(R.id.monday)
+
+        //set up onclicklistener
+
+        monday.setOnClickListener {
+            val mondayIntent = Intent(this, MondayActivity::class.java)
+            startActivity(mondayIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/android/checkmeals/MondayActivity.kt
@@ -0,0 +1,14 @@
+package com.example.android.checkmeals
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MondayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_days)
+    }
+}
