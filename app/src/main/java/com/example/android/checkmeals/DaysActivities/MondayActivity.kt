462bc2f0449cc5d6cd3b50d62e9e07d41729940b
Akash <akashmkabra@gmail.com>
2019-08-04 19:10:21 -0400

    Created a custom ListView with a custom ArrayAdapter that takes care of CheckBoxes and then shows up in the ListView

--- app/src/main/java/com/example/android/checkmeals/DaysActivities/MondayActivity.kt
+++ app/src/main/java/com/example/android/checkmeals/DaysActivities/MondayActivity.kt
@@ -2,14 +2,22 @@ package com.example.android.checkmeals.DaysActivities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.app.ActionBar
+import android.widget.ListView
 import com.example.android.checkmeals.R
+import com.example.android.checkmeals.listAdapter
 
 class MondayActivity : AppCompatActivity() {
+
+    val mealNames = arrayListOf<String>("Hello", "Test")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_days)
+
+        val mylistAdapter = listAdapter(this, mealNames)
+        var listView = findViewById(R.id.list) as ListView
+        listView.adapter = mylistAdapter
+
 
 
     }
---
+++ app/src/main/java/com/example/android/checkmeals/listAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.android.checkmeals
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.CheckBox
+import android.widget.TextView
+
+class listAdapter(context: Context, val list: ArrayList<String>):
+        ArrayAdapter<String>(context, R.layout.checkbox_list, list) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val rowView = inflater.inflate(R.layout.checkbox_list, parent, false)
+
+        val mealName = rowView.findViewById(R.id.checkbox) as CheckBox
+
+        mealName.text = list[position]
+
+
+        return rowView
+    }
+
+
+
+}
