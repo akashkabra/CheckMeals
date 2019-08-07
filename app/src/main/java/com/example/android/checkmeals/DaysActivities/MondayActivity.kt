ffcdeb319489c217ed27eabd974b5ee575ab4a86
Akash <akashmkabra@gmail.com>
2019-08-06 21:37:59 -0400

    Need to delete popupwindow as it is not working according to plans and need to create new menu options at different places

--- app/src/main/java/com/example/android/checkmeals/DaysActivities/MondayActivity.kt
+++ app/src/main/java/com/example/android/checkmeals/DaysActivities/MondayActivity.kt
@@ -25,8 +25,6 @@ class MondayActivity : AppCompatActivity() {
         var listView = findViewById(R.id.list) as ListView
         listView.adapter = mylistAdapter
 
-
-
     }
 
     // Inflate the menu options from the XML file.
@@ -40,47 +38,55 @@ class MondayActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         item?: return false
         return when (item.itemId) {
-            R.id.addMeals -> {
+            R.id.addmeals -> {
                 // For now, just show a toast message to make sure it works.
                 // TODO: Make a XML + Kotlin file for this add meal setting and create an intent to go to that page.
                 // TODO: Or make it so it pops up a window in which the user enters the names of the meals.
                 val toast = Toast.makeText(applicationContext, "Adding Meals...", Toast.LENGTH_SHORT)
                 toast.show()
+//
+//                val inflater:LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+//                val view = inflater.inflate(R.layout.popup_edittext, null)
+//                val popupWindow = PopupWindow(
+//                        view,
+//                        LinearLayout.LayoutParams.WRAP_CONTENT,
+//                        LinearLayout.LayoutParams.WRAP_CONTENT
+//                )
+//                popupWindow.setFocusable(true)
+//                popupWindow.update()
+//
+//
+//                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                    popupWindow.elevation = 10.0F
+//                }
+//                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                    val slideIn = Slide()
+//                    slideIn.slideEdge = Gravity.TOP
+//                    popupWindow.enterTransition = slideIn
+//
+//                    val slideOut = Slide()
+//                    slideOut.slideEdge = Gravity.RIGHT
+//                    popupWindow.exitTransition = slideOut
+//                    popupWindow.dismiss()
+//                }
+//
+//                TransitionManager.beginDelayedTransition(root_layout)
+//                popupWindow.showAtLocation(
+//                        root_layout,
+//                        Gravity.CENTER,
+//                        0,
+//                        0
+//                )
+//                val btn = findViewById<Button>(R.id.btn_done) as Button
+//                btn.setOnClickListener{
+//                    Toast.makeText(applicationContext, "Hello", Toast.LENGTH_SHORT).show()
+//                }
 
-                val inflater:LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-                val view = inflater.inflate(R.layout.popup_edittext, null)
-                val popupWindow = PopupWindow(
-                        view,
-                        LinearLayout.LayoutParams.WRAP_CONTENT,
-                        LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-                popupWindow.setFocusable(true)
-                popupWindow.update()
-
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    popupWindow.elevation = 10.0F
-                }
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    val slideIn = Slide()
-                    slideIn.slideEdge = Gravity.TOP
-                    popupWindow.enterTransition = slideIn
-
-                    val slideOut = Slide()
-                    slideOut.slideEdge = Gravity.RIGHT
-                    popupWindow.exitTransition = slideOut
-                    popupWindow.dismiss()
-                }
-
-                TransitionManager.beginDelayedTransition(root_layout)
-                popupWindow.showAtLocation(
-                        root_layout,
-                        Gravity.CENTER,
-                        0,
-                        0
-                )
 
                 true
             } else -> super.onOptionsItemSelected(item)
         }
     }
+
+
 }
