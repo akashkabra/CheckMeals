8317be4ffa37c3846172e3f0fb8836a4ab43c68e
Akash <akashmkabra@gmail.com>
2019-08-17 23:28:41 -0400

    Internetal Storage works perfectly with everything being edited at once

--- app/src/main/java/com/example/android/checkmeals/DaysActivities/MondayActivity.kt
+++ app/src/main/java/com/example/android/checkmeals/DaysActivities/MondayActivity.kt
@@ -4,67 +4,67 @@ import android.content.Context
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.transition.Slide
 import android.transition.TransitionManager
+import android.util.Log
 import android.view.*
 import android.widget.*
 import com.example.android.checkmeals.R
 import com.example.android.checkmeals.listAdapter
 import kotlinx.android.synthetic.main.activity_days.*
 import kotlinx.android.synthetic.main.popup_edittext.*
-import org.w3c.dom.Text
 import java.io.*
 import java.lang.Exception
 import java.lang.StringBuilder
 
 class MondayActivity : AppCompatActivity() {
 
-    var mealNames = arrayListOf<String>("Hello", "Test")
-    var placeHolder = ""
-    var value = ""
+    //    TODO: The whole SQLite database implementation
 
+    var mealNames = arrayListOf<String>("Hello", "Test")
+
+//    TODO: REMOVE ALL FINDVIEWBYID, REPLACE USING LAMBDA. Makes code more precise since kotlin allows us to omit findviewbyid.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_days)
+
+//    TODO: Make sure to call fileOutput() to make sure if user input previous data or not.
 
         val mylistAdapter = listAdapter(this, mealNames)
         var listView = findViewById(R.id.list) as ListView
         listView.adapter = mylistAdapter
 
+
+//        Set up OnClickListener for each textview macros so user can edit directly from there
+//        TODO: Remove findviewbyid and directly use onclicklistener using ID names! This is the better Kotlin (Lambda) way!
         val cal = findViewById<TextView>(R.id.calories)
         cal.setOnClickListener {
-            var setLine = "Enter amount of Calories"
+            var setLine = "Enter your macros"
             var setEdit = "Calories"
-            val id = R.id.calories
-//            TODO: Delete the return value from popup() because we aren't using it --> also remove it from the function implementation
-            value = popup(setLine, setEdit, id)
+            popup(setLine, setEdit)
         }
 
         val macroFats = findViewById<TextView>(R.id.fats)
         macroFats.setOnClickListener {
-            var setLine = "Enter the amount of fats in grams"
-            var setEdit = "Fats"
-            val id = R.id.fats
-            popup(setLine, setEdit, id)
+            var setLine = "Enter your macros"
+            var setEdit = "Calories"
+            popup(setLine, setEdit)
         }
 
         val macroCarbs = findViewById<TextView>(R.id.carbs)
         macroCarbs.setOnClickListener {
-            var setLine = "Enter the amount of carbs in grams"
-            var setEdit = "Carbs"
-            val id = R.id.carbs
-            popup(setLine, setEdit, id)
+            var setLine = "Enter your macros"
+            var setEdit = "Calories"
+            popup(setLine, setEdit)
         }
 
         val macroProtein = findViewById<TextView>(R.id.protein)
         macroProtein.setOnClickListener {
-            var setLine = "Enter the amount of protein in grams"
-            var setEdit = "Protein"
-            val id = R.id.protein
-            popup(setLine, setEdit, id)
+            var setLine = "Enter your macros"
+            var setEdit = "Calories"
+            popup(setLine, setEdit)
         }
-
-
     }
 
     // Inflate the menu options from the XML file.
@@ -79,25 +79,26 @@ class MondayActivity : AppCompatActivity() {
         item?: return false
         return when (item.itemId) {
             R.id.addmeals -> {
-                // For now, just show a toast message to make sure it works.
-                // TODO: Make a XML + Kotlin file for this add meal setting and create an intent to go to that page.
-                // TODO: Or make it so it pops up a window in which the user enters the names of the meals.
+//                TODO: Make a new POPUP() function and use this popupwindow to ADD MEALS into the database and then put into the checkbox list!
 //                val toast = Toast.makeText(applicationContext, "Adding Meals...", Toast.LENGTH_SHORT)
 //                toast.show()
-//              TODO: Delete the return value from popup() because we aren't using it --> also remove it from the function implementation
-                value = popup("test","hold", 1)
+//                TODO: NEED NEW POPUP FUNCTION AS ITS VERY DIFFERENT
+                popup("test","hold")
                 true
             }
+//            Just testing purposes, TODO: Delete this!
             R.id.test -> {
-//                Toast.makeText(this, "test: " + placeHolder, Toast.LENGTH_SHORT).show()
+//                Toast.makeText(this, "test: ", Toast.LENGTH_SHORT).show()
                 fileOutput()
                 true
             } else -> super.onOptionsItemSelected(item)
         }
     }
 
-//    Pop-up-Window
-    private fun popup(text: String, edit: String, id: Int): String {
+//    Pop-up-Window used for setting up macros
+//    @param text -- A string variable which says what the popup window is for
+//    @param edit -- A string variable which shows the hint behind edittext
+    private fun popup(text: String, edit: String) {
         val inflater:LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view = inflater.inflate(R.layout.popup_edittext, null)
         val popupWindow = PopupWindow(
@@ -108,10 +109,11 @@ class MondayActivity : AppCompatActivity() {
         popupWindow.setFocusable(true)
         popupWindow.update()
 
-
+//        Sets up elevation for popup window
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             popupWindow.elevation = 10.0F
         }
+//        Only if above API level 23
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             val slideIn = Slide()
             slideIn.slideEdge = Gravity.TOP
@@ -123,30 +125,45 @@ class MondayActivity : AppCompatActivity() {
 //            Toast.makeText(applicationContext, "Done!", Toast.LENGTH_SHORT).show()
         }
 
-//      Set the texts for the popup view
+//        Set the text and hint for the popup view
         val changeText = view.findViewById<TextView>(R.id.popup)
         changeText.setText(text)
         val changeEdit = view.findViewById<EditText>(R.id.editMe)
         changeEdit.setHint(edit)
 
-//      Change R.id.calories to get from @params so it works for everything
-        val macros = findViewById<TextView>(id)
+//        Make new EDITTEXT and button and get their IDs
+        val macroFatsID = addEditText(view, "Fats")
+        val macroCarbsID = addEditText(view, "Carbs")
+        val macroProteinID = addEditText(view, "Protein")
+        val btnDone = addBtn(view, "Done")
 
+//        Get references to the views
         val mealInput = view.findViewById<EditText>(R.id.editMe)
-        val btnClose = view.findViewById<Button>(R.id.btndone)
+        val macroFats = view.findViewById<EditText>(macroFatsID)
+        val macroCarbs = view.findViewById<EditText>(macroCarbsID)
+        val macroProtein = view.findViewById<EditText>(macroProteinID)
+
+
+//        Set onclicklistener to close the popup-window, update values, and store into .txt file internally
+        val btnClose = view.findViewById<Button>(btnDone)
         btnClose.setOnClickListener {
             popupWindow.dismiss()
-            val stringBuilder: StringBuilder = StringBuilder()
-            stringBuilder.append(edit + ": ")
 
-            placeHolder = mealInput.text.toString()
+            val cals = mealInput.text.toString()
+            val fats = macroFats.text.toString()
+            val carbs = macroCarbs.text.toString()
+            val protein = macroProtein.text.toString()
 
-            stringBuilder.append(placeHolder + " \n")
-            macros.setText(placeHolder)
-            fileInput(stringBuilder)
-//            Toast.makeText(this, placeHolder, Toast.LENGTH_SHORT).show()
+            findViewById<TextView>(R.id.calories).setText(cals)
+            findViewById<TextView>(R.id.fats).setText(fats)
+            findViewById<TextView>(R.id.carbs).setText(carbs)
+            findViewById<TextView>(R.id.protein).setText(protein)
+
+            val finalData = "Calories: " + cals + "\n" + "Fats: " + fats + "\n" + "Carbs: " + carbs + "\n" + "Protein: " + protein + "\n"
+            fileInput(finalData)
         }
 
+//        Show the popup window on top of the main root layout
         TransitionManager.beginDelayedTransition(root_layout)
         popupWindow.showAtLocation(
                 root_layout,
@@ -154,15 +171,47 @@ class MondayActivity : AppCompatActivity() {
                 0,
                 0
         )
-        return placeHolder
     }
 
-    private fun fileInput(data: StringBuilder) {
+//    Add new EditText inside the popup window
+//    @param view -- reference to the layout
+//    @param hint -- A string which is the hint for EditText
+    fun addEditText(view: View, hint: String): Int {
+        val linearLayout = view.findViewById<LinearLayout>(R.id.newWindow)
+        val editText = EditText(this)
+        editText.setHint(hint)
+        editText.inputType = InputType.TYPE_CLASS_NUMBER
+        editText.id = View.generateViewId()
+        editText.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        linearLayout.addView(editText)
+        return editText.id
+    }
+
+//    Add new Button inside the popup window
+//    @param view -- reference to the layout
+//    @param text -- A string which is the text inside Button
+    fun addBtn(view: View, text: String): Int {
+        val linearLayout = view.findViewById<LinearLayout>(R.id.newWindow)
+        val btn = Button(this)
+        btn.text = text
+        btn.id = View.generateViewId()
+        btn.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        linearLayout.addView(btn)
+        return btn.id
+    }
+
+//    Create new or open if already created a file and input the data given by user
+//    @param data -- A String which includes name of macro and amount of grams
+    private fun fileInput(data: String) {
         val fileOutputStream: FileOutputStream
         val file = "macroDays.txt"
         try {
-            fileOutputStream = openFileOutput(file, Context.MODE_APPEND)
-            fileOutputStream.write(data.toString().toByteArray())
+//            MODE_PRIVATE creates a new file and deletes old file if there
+//            MODE_APPEND adds onto the previous file
+            fileOutputStream = openFileOutput(file, Context.MODE_PRIVATE)
+            fileOutputStream.write(data.toByteArray())
         } catch (e: FileNotFoundException) {
             e.printStackTrace()
             Toast.makeText(applicationContext, "File Not Found", Toast.LENGTH_SHORT).show()
@@ -173,22 +222,21 @@ class MondayActivity : AppCompatActivity() {
         Toast.makeText(applicationContext, "Data saved in file!", Toast.LENGTH_SHORT).show()
     }
 
+//    Read the data from the file and fill the macros when activity loads if user previously entered the data
 //    TODO: Put openFileInput into a try and catch block to make sure the app doesn't crash
+//    TODO: Call this function in the beginning and manipulate the string into breaking it by macros and filling in data for user,
     private fun fileOutput() {
-        var fileInputStream: FileInputStream? = null
+        var fileInputStream: FileInputStream
         val file = "macroDays.txt"
         fileInputStream = openFileInput(file)
-        var inputSteamReader: InputStreamReader = InputStreamReader(fileInputStream)
-        val bufferReader: BufferedReader = BufferedReader(inputSteamReader)
-        val stringBuilder: StringBuilder = StringBuilder()
+        var inputSteamReader = InputStreamReader(fileInputStream)
+        val bufferReader = BufferedReader(inputSteamReader)
+        val stringBuilder = StringBuilder()
         var text: String? = null
         while ( { text = bufferReader.readLine(); text }() != null) {
             stringBuilder.append(text + "\n")
+            Log.i("TAG", "|" + text + "|")
         }
         outputHolder.setText(stringBuilder)
-//        Toast.makeText(applicationContext, stringBuilder, Toast.LENGTH_SHORT).show()
-
      }
-
-
 }
